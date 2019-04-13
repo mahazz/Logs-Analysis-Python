20750157218d2b60f3c450f4967f99c5797f0e31
Maha Alzahrani <36902126+mahazz@users.noreply.github.com>
2019-04-13 13:49:00 +0300

    Update logs_analysis.py

--- logs_analysis.py
+++ logs_analysis.py
@@ -8,20 +8,37 @@ def main():
     cursor = db.cursor()
     # Q1
     cursor.execute("""SELECT title,  count(*) as views FROM articles a, log l WHERE a.slug = substring(l.path, 10) GROUP BY title ORDER BY views DESC LIMIT 3;""")
+    print("Most popular authors:")
     results = cursor.fetchall()
     print results
-    
+
     # Q2
-    cursor.execute("""SELECT name,  count(*) as views FROM authors a, log l WHERE a.bio = substring(l.path, 10) GROUP BY name ORDER BY views DESC LIMIT 3;""")
-    results = cursor.fetchall()
-    print results
-    
+    sql_popular_authors = """
+    SELECT article_view.name, SUM(article_view.view) AS author_view
+    FROM article_view
+    GROUP BY article_view.name
+    ORDER BY author_view DESC;
+    """
+    cursor.execute(sql_popular_authors)
+    print("Most popular authors:")
+    for (name, view) in cursor.fetchall():
+        print("    {} - {} views".format(name, view))
+    print("-" * 70)
+
     # Q3
-    cursor.execute("""SELECT date(log.time), count(log.status) * 100 / subquery1.error_instance AS error FROM (SELECT date(log.time), count(log.status) AS error_instance FROM log GROUP BY log.time ORDER BY error_instance desc) subquery1 JOIN log ON date(log.time) = subquery1.date GROUP BY log.time HAVING status='404 NOT FOUND' ORDER BY error;""")
-    results = cursor.fetchall()
-    print results
-    
-    
+    sql_more_than_one_percent_errors = """
+    SELECT *
+    FROM error_rate
+    WHERE error_rate.percentage > 1
+    ORDER BY error_rate.percentage DESC;
+    """
+    cursor.execute(sql_more_than_one_percent_errors)
+    print("Days with more than 1% errors:")
+    for (date, percentage) in cursor.fetchall():
+        print("    {} - {}% errors".format(date, percentage))
+    print("-" * 70)
+
+
     # Close connection with the database
     cursor.close()
     db.close()
