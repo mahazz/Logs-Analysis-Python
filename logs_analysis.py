17178fb38d6fd7be59de53a62f9dddff5c4ffac6
Maha Alzahrani <36902126+mahazz@users.noreply.github.com>
2019-04-19 00:21:27 +0300

    Update logs_analysis.py

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,46 +1,81 @@
-
-#!/usr/bin/env python2
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import psycopg2
 
 
-def database_connect("news"): """
-Connect to the database.  Returns a database connection.
+def database_connect(news):
+    """
+Connect to the database.
 """
+
+
 try:
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    return db, c
-except psycopg2.Error as e:
-        print ("Unable to connect to database")
-        sys.exit(1)
-    db = psycopg2.connect("dbname=news")
+    db = psycopg2.connect('dbname=news')
+    cursor = db.cursor()
+    return (db, cursor)
+except psycopg2.Error, error:
+    print 'Unable to connect to database'
+    sys.exit(1)
+
+    # db = psycopg2.connect(news)
 # cursor
+
 cursor = db.cursor()
-def print_top_articles(): """
-Prints out the top 3 articles of all time.
-"""
-    query = "SELECT title,  count(*) as views FROM articles a, log l WHERE a.slug = substring(l.path, 10) GROUP BY title ORDER BY views DESC LIMIT 3;"
-    results = execute_query(query)
 
-def print_top_authors(): """
-Prints a list of authors ranked by article views.
-"""
-    query = "SELECT article_view.name, SUM(article_view.view) AS author_view FROM article_view GROUP BY article_view.name ORDER BY author_view DESC;"
-    results = execute_query(query)
 
-def print_errors_over_one(): """
-Prints out the days where more than 1% of logged access requests were errors.
+# Q1
+
+def print_top_three_articles():
+    """
+The most popular three articles of all time:
 """
-    query = "select to_char(date, 'FMMonth FMDD, YYYY'), err/total as ratio from (select time::date as date, count(*) as total, sum((status != '200 OK')::int)::float as err from log group by date) as errors where err/total > 0.01;"
-    results = execute_query(query)
-    
+
+
+query = \
+    """select articles.title , COUNT(log.path) AS views from articles, log
+    WHERE articles.slug = substring(log.path, 10, 23) group by articles.title
+    order by views DESC LIMIT 3;"""
+results = execute_query(query)
+
+
+# Q2
+
+def print_top_authors():
+    """
+The most popular article authors of all time:
+"""
+
+
+query = \
+    """select authors.name , COUNT(log.path) AS views from authors, log
+    group by authors.name order by views DESC;"""
+results = execute_query(query)
+
+
+# Q3
+
+def print_errors_over_one():
+    """
+The days did more than 1% of requests lead to error:
+"""
+
+
+query = \
+    """select to_char(total_all_status.date, 'FMMonth FMDD, YYYY') AS date,
+    errors_requests.requests from errors_requests, total_all_status
+    where errors_requests.requests > 1
+    group by total_all_status.date, errors_requests.requests
+    order by errors_requests.requests DESC;"""
+results = execute_query(query)
+
 if __name__ == '__main__':
-    print_top_articles()
+    print_top_three_articles()
     print_top_authors()
     print_errors_over_one()
 
     # Close connection with the database
+
     cursor.close()
     db.close()
-    connection.close();
+    connection.close()
